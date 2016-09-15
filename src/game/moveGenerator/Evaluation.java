e3f9e12d0fd155b19067856f1fb5433b416ac0dd
Benry8 <blacky8@hotmail.it>
2016-09-15 20:51:33 +0200

    add Evaluation.java to repository

---
+++ src/game/moveGenerator/Evaluation.java
@@ -0,0 +1,13 @@
+package game.moveGenerator;
+
+/**
+ * Created by enry8 on 15/09/16.
+ */
+public class Evaluation {
+
+    private Integer choice = 0;
+
+    public static Integer getEvaluationValue(Node node, Integer choice){
+        return 1;
+    }
+}
